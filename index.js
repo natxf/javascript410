ec556ab6a0c367007d6b8eaf6e93c00144cf7b9f
natxf <157922167+natxf@users.noreply.github.com>
2024-11-21 08:13:15 +0000

    zadanie 4.1.1

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const app = express();
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
 //TODO1  
+  const r = req.params.r;
+  const a = (Math.PI * r * r).toFixed(2);
+  const c = (2 * Math.PI * r).toFixed(2);
+  const result = {
+    area: a,
+    circumference: c  
+  };
   res.json(result);
 });
+
+app.get('/math/rectangle/2/3')
 
 //TODO2
 
